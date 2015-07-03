c6502d5e04acb51486575c459355198bfb8f31ec
Dean Montgomery <montgomery.dean97@gmail.com>
2015-07-03 19:21:13 +0100

    simplify retry_requests function

--- elvanto_sync/utils.py
+++ elvanto_sync/utils.py
@@ -14,27 +14,13 @@ def clean_emails(elvanto_emails=(), google_emails=()):
     return emails
 
 
-def retry_request(url, http_method, params=None, data=None, json=None, headers=None, auth=None):
+def retry_request(url, http_method, *args, **kwargs):
     assert http_method in ['get', 'post', 'delete', 'patch', 'put']
     MAX_TRIES = 3
-
-    r_kwargs = {}
-    if params is not None:
-        r_kwargs['params'] = params
-    if data is not None:
-        r_kwargs['data'] = data
-    if json is not None:
-        r_kwargs['json'] = json
-    if headers is not None:
-        r_kwargs['headers'] = headers
-    if auth is not None:
-        r_kwargs['auth'] = auth
-
     r_func = getattr(requests, http_method)
-
     tries = 0
     while True:
-        resp = r_func(url, **r_kwargs)
+        resp = r_func(url, *args, **kwargs)
         if resp.status_code != 200 and tries < MAX_TRIES:
             tries += 1
             continue
